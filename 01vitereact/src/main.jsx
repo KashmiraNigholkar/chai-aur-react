bcc1f69e2990fad6b82dde069863f034b920973e
Kashmira Nigholkar <kashmiranigholkar421@gmail.com>
2025-04-12 11:05:00 +0530

    Add files via upload

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,48 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 
+// function MyApp(){
+//   return (
+//     <div>
+//       <h1>Custom App !| chai</h1>
+//     </div>
+//   )
+// }
+
+// const ReactElement={
+//   type:'a',
+//   props:{
+//     href:'https://google.com',
+//     terget:'_blank',
+
+//   },
+//   children:'Click me to visit google'
+// }
+
+// const anotherElement =(
+//   <a href="https://google.com" target='_blank'>Visit google</a>
+// )
+
+const anotherUser="chai aur codde"
+
+const reactElement=React.createElement(
+  'a',
+  {href:'http://google.com',target:'_blank'},
+  'click me to visit google',
+  anotherUser
+
+)
+ 
+
+
+
+
+
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ 
+reactElement
+/* <App/>  */
+  
 )
